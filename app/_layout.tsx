ccc5a73f5fec66cae7e4f42c788142edcfec3e8f
MatiriSimon <symonmatyry@gmail.com>
2026-04-19 09:35:27 +0000

    Setting up auth with supabase

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,41 @@
-// import { useEffect } from "react";
-import { Stack } from "expo-router";
-//import { testSupabaseConnection } from "../utils/checkConnection";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
+
+import { SplashScreenController } from '@/components/splash-screen-controller'
+
+import { useAuthContext } from '@/hooks/use-auth-context'
+import { useColorScheme } from '@/hooks/use-color-scheme'
+import AuthProvider from '@/providers/auth-provider'
+
+// Separate RootNavigator so we can access the AuthContext
+function RootNavigator() {
+  const { isLoggedIn } = useAuthContext()
+
+  return (
+    <Stack>
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  )
+}
 
 export default function RootLayout() {
-  /*
-  console.log("--- LAYOUT RENDERED ---"); // This should show up instantly
+  const colorScheme = useColorScheme()
 
-  useEffect(() => {
-    console.log("--- LAYOUT MOUNTED (useEffect) ---");
-    //testSupabaseConnection();
-  }, []);
-*/
-  return <Stack />;
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AuthProvider>
+        <SplashScreenController />
+        <RootNavigator />
+        <StatusBar style="auto" />
+      </AuthProvider>
+    </ThemeProvider>
+  )
 }
